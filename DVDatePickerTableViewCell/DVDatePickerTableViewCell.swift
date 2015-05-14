0b0cd31844176ac8e994cea6b78667d903d2ca75
Dylan Vann <dylan.vann@gmail.com>
2015-05-14 05:16:53 -0400

    Hid color lock view.

--- DVDatePickerTableViewCell/DVDatePickerTableViewCell.swift
+++ DVDatePickerTableViewCell/DVDatePickerTableViewCell.swift
@@ -9,29 +9,28 @@
 import Foundation
 import UIKit
 
-// DVColorLockView ovverides the backgroundColor setter for UIView so that highlighting a UITableViewCell won't change the color of its DVColorLockView subviews (Highlighting a UITableViewCell changes the background color of all subviews, it's annoying.)
-class DVColorLockView:UIView {
-    
-    var lockedBackgroundColor:UIColor {
-        set {
-            super.backgroundColor = newValue
-        }
-        get {
-            return super.backgroundColor!
-        }
-    }
-    
-    override var backgroundColor:UIColor? {
-        set {
-        }
-        get {
-            return super.backgroundColor
-        }
-    }
-    
-}
-
 class DVDatePickerTableViewCell: UITableViewCell {
+    
+    class DVColorLockView:UIView {
+        
+        var lockedBackgroundColor:UIColor {
+            set {
+                super.backgroundColor = newValue
+            }
+            get {
+                return super.backgroundColor!
+            }
+        }
+        
+        override var backgroundColor:UIColor? {
+            set {
+            }
+            get {
+                return super.backgroundColor
+            }
+        }
+        
+    }
     
     // Class variable workaround.
     struct Stored {
